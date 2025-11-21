9a0c1b4bdec68f0ad376b8601c682aa476f8f2be
nrjbwj <neeraj@nrjbwj.xyz>
2025-11-21 17:50:19 +0530

    fix: use Material UI AppRouterCacheProvider to fix hydration issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { ReduxProvider } from "@/components/providers/ReduxProvider";
 import { QueryProvider } from "@/components/providers/QueryProvider";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
@@ -27,14 +28,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* AppRouterCacheProvider ensures Material UI styles are properly collected on the server
+          and appended to <head> instead of <body>, preventing hydration mismatches.
+          See: https://mui.com/material-ui/integrations/nextjs/ */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ReduxProvider>
-          <QueryProvider>
-            <ThemeProvider>{children}</ThemeProvider>
-          </QueryProvider>
-        </ReduxProvider>
+        <AppRouterCacheProvider>
+          <ReduxProvider>
+            <QueryProvider>
+              <ThemeProvider>{children}</ThemeProvider>
+            </QueryProvider>
+          </ReduxProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
